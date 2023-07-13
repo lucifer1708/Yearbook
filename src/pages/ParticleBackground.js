e27705263e6cad50564b8d74d31ddc9ac45b021c
lucifer1708 <20bce091@nith.ac.in>
2023-07-13 15:09:55 +0530

    changes

--- src/pages/ParticleBackground.js
+++ src/pages/ParticleBackground.js
@@ -1,29 +1,41 @@
 import React from "react";
-import Particles from "react-particles-js";
+import Particles from "react-particles";
 
-function ParticleBackground(){
-    return(
-    <Particles 
-        style={{position:"fixed", zIndex:"-1", height:"100%", width:"100%"}}
-        params={{
-        "particles": {
-            "number": {
-                "value": 50
-            },
-            "size": {
-                "value": 3
-            }
-        },
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                }
-            }
-      }
-    }}/>
-  )
+function ParticleBackground() {
+     return (
+          <Particles
+               style={{ position: "fixed", zIndex: "-1", height: "100%", width: "100%" }}
+               params={{
+                    particles: {
+                         number: {
+                              value: 200,
+                              density: {
+                                   enable: true,
+                                   value_area: 1000,
+                              }
+                         },
+                    },
+               }}
+          // params={{
+          //      "particles": {
+          //           "number": {
+          //                "value": 50
+          //           },
+          //           "size": {
+          //                "value": 3
+          //           }
+          //      },
+          //      "interactivity": {
+          //           "events": {
+          //                "onhover": {
+          //                     "enable": true,
+          //                     "mode": "repulse"
+          //                }
+          //           }
+          //      }
+          // }}
+          />
+     )
 }
 
 export default ParticleBackground;
